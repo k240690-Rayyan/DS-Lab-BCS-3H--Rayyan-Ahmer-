eebe17c3c0471160012ade74a122bb668548ec94
Rayyan Ahmer <k240690@nu.edu.pk>
2025-09-28 22:55:59 +0500

    Create Task 4.cpp

---
+++ Lab 5/Task 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int ackermann(int m, int n) {
+    if(m==0)    
+      return n+1;
+    else if(n==0)   
+      return ackermann(m-1,1);
+    else   
+      return ackermann(m-1, ackermann(m, n-1));
+}
+
+int main() {
+    cout << ackermann(7,4);
+}
